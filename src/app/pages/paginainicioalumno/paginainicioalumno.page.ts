aafa71b21a43941b45163d59dcb788cdce28fa8d
jassschaff <j.schaff@duocuc.cl>
2021-09-21 20:29:53 -0300

    first commit

---
+++ src/app/pages/paginainicioalumno/paginainicioalumno.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paginainicioalumno',
+  templateUrl: './paginainicioalumno.page.html',
+  styleUrls: ['./paginainicioalumno.page.scss'],
+})
+export class PaginainicioalumnoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/paginainiciodocente/paginainiciodocente.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paginainiciodocente',
+  templateUrl: './paginainiciodocente.page.html',
+  styleUrls: ['./paginainiciodocente.page.scss'],
+})
+export class PaginainiciodocentePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
